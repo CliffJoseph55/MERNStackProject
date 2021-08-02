76e59f80005c259c2f20ae671653a4c50cbb4e1b
Cliff Joseph <cliffjoseph55@gmail.com>
2021-08-02 14:26:54 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Posts from './components/Posts/posts.js';
+import Form from './components/Forms/form.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Container, Typography, AppBar , Grid , Grow} from '@material-ui/core';
+import logo from './images/logo.png';
+const App = () => {
+    
+    return(
+
+   <Container maxwidth = "lg">
+       <AppBar position = "static" color = "inherit">
+           <Typography variant = "h2" align = "center"> Get Money Baby</Typography>
+           <img src = {logo} alt = "money" height = "60"/>
+       </AppBar>
+       <Grow in>
+           <Container>
+               <Grid container justify = 'space-between' alignItems = 'stretch' spacing = {3}>
+                   <Grid item xs = {12} sm = {7}>
+                       <Posts />
+                   </Grid>
+                   <Grid item xs = {12} sm = {4}>
+                       <Form />
+                   </Grid>
+
+               </Grid>
+
+           </Container>
+
+       </Grow>
+
+   </Container>
+
+    );
 }
 
 export default App;
